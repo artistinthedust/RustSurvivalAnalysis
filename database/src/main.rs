5ec1687694b0b611d357513e70770de05f539acf
Jung-Hua Liu <jung-hua.liu@ocrlabs.com>
2023-07-16 16:33:23 +0800

    new service

--- database/src/main.rs
+++ database/src/main.rs
@@ -11,6 +11,19 @@ struct Author {
     country: String
 }
 
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
 fn main() {
     let mut rng = rand::thread_rng();
 
--- web/src/main.rs
+++ web/src/main.rs
@@ -1,9 +1,21 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
+// This struct represents state
+struct AppState {
+    app_name: String,
+}
+
+#[get("/")]
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name; // <- get app_name
+    format!("Hello {app_name}!") // <- response with app_name
+}
+/*
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
+*/
 
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
@@ -18,6 +30,17 @@ async fn manual_hello() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Actix Web"),
+            }))
+            .service(index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+    /*
+    HttpServer::new(|| {
+        App::new()
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
@@ -25,4 +48,5 @@ async fn main() -> std::io::Result<()> {
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
+    */
 }
